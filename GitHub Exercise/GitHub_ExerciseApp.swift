70f59e3fff2949075db15af146f40aeecc45a519
Paul Hancock <paul@hancockfamily.com>
2024-09-23 14:28:41 -0500

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Paul Hancock on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHub ExerciseTests/GitHub_ExerciseTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseTests.swift
+//  GitHub ExerciseTests
+//
+//  Created by Paul Hancock on 9/23/24.
+//
+
+import Testing
+@testable import GitHub_Exercise
+
+struct GitHub_ExerciseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
